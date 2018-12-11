3408c251927fecb94d29e54c650162bb9d71e8d9
Lucas Russo <lerwys@gmail.com>
2018-12-11 10:00:52 -0200

    functions.sh: collapse both full/shallow_repos to a single one
    This increases modularoty and eases maintenance.

--- functions.sh
+++ functions.sh
@@ -12,38 +12,50 @@ dest_proj_name()
     echo ${DEST_PROJ_NAME}
 }
 
-shallow_repo()
+get_repo()
 {
     GIT_URL=$1
     GIT_ORG=$2
     PROJECT_NAME=$3
     TAG=$4
     DEST_DIR=$5
+    TYPE=$6
 
     DEST_PROJ_NAME=$(dest_proj_name ${PROJECT_NAME} ${TAG})
+    FULL_GIT_URL=${GIT_URL}/${GIT_ORG}/${PROJECT_NAME}
+    DEST=${DEST_DIR}/${DEST_PROJ_NAME}
 
-    echo "Grabbing ${PROJECT_NAME} at tag: ${TAG}"
+    # Do git submodule init/update if not available
+    if [ -z "$(find ${DEST} -type d -name ".git" 2>/dev/null)" ]; then
+        if [  "${TYPE}" == "shallow" ]; then
+            echo "Grabbing ${PROJECT_NAME} \"shallow\" repository at tag: ${TAG}"
+            git clone -q --branch ${TAG} --depth 1 ${FULL_GIT_URL} ${DEST}
+        elif [  "${TYPE}" == "full" ]; then
+            echo "Grabbing ${PROJECT_NAME} \"full\" repository at tag: ${TAG}"
+            git clone -q ${FULL_GIT_URL} ${DEST}
+            ( \
+                cd ${DEST} && \
+                git checkout -q ${TAG}
+            )
+        fi
+    else
+        echo "Repository ${PROJECT_NAME} already exists. Updating the current one to tag: ${TAG}"
+        ( \
+            cd ${DEST} && \
+            git fetch -q --all && \
+            git checkout -q ${TAG} && \
+            git reset -q --hard origin/${TAG}
+        )
+    fi
+}
 
-    git clone -q --branch ${TAG} --depth 1 ${GIT_URL}/${GIT_ORG}/${PROJECT_NAME} ${DEST_DIR}/${DEST_PROJ_NAME}
+
+shallow_repo()
+{
+    get_repo "${@:1:5}" "shallow"
 }
 
 full_repo()
 {
-    GIT_URL=$1
-    GIT_ORG=$2
-    PROJECT_NAME=$3
-    TAG=$4
-    DEST_DIR=$5
-
-    DEST_PROJ_NAME=$(dest_proj_name ${PROJECT_NAME} ${TAG})
-
-    echo "Grabbing ${PROJECT_NAME} at tag: ${TAG}"
-
-    git clone -q ${GIT_URL}/${GIT_ORG}/${PROJECT_NAME} ${DEST_DIR}/${DEST_PROJ_NAME}
-
-    CURR=$(pwd)
-
-    cd ${DEST_PROJ_NAME}
-    git checkout -q ${TAG}
-    cd "${CURR}"
+    get_repo "${@:1:5}" "full"
 }
